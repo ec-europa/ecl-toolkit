51ead0a65e42d4655efbf5269b4919b5e550d203
Yannick Huard <mail@yannickhuard.com>
2017-05-19 15:56:38 +0200

    Enhance SCSS resolver

--- packages/ecl-builder/scripts/styles.js
+++ packages/ecl-builder/scripts/styles.js
@@ -8,27 +8,31 @@ const mkdirp = require('mkdirp');
 const findup = require('findup-sync');
 
 module.exports = (entry, dest, options) => {
-  const params = process.env.NODE_ENV === 'production' ? {
-    plugins: [cssnano],
-  } : {
-    plugins: [autoprefixer],
-  };
+  const params = process.env.NODE_ENV === 'production'
+    ? {
+        plugins: [cssnano],
+      }
+    : {
+        plugins: [autoprefixer],
+      };
 
-  sass.render({
-    file: entry,
-    importer: (url, prev, done) => {
-      if (url.startsWith('~')) {
+  sass.render(
+    {
+      file: entry,
+      importer: (url, prev, done) => {
+        // if (url.startsWith('~')) {
         const base = path.dirname(prev);
-        const normalizedUrl = url.substr(1);
+        const normalizedUrl = url.startsWith('~') ? url.substr(1) : url;
         const normalizedDir = path.dirname(normalizedUrl);
         const normalizedFile = path.basename(normalizedUrl, '.scss');
+        const prefix = url.startsWith('~') ? 'node_modules' : '';
 
         const checkFor = [
-          path.join('node_modules', normalizedDir, `${normalizedFile}.scss`),
-          path.join('node_modules', normalizedDir, `_${normalizedFile}.scss`),
-          path.join('node_modules', normalizedUrl, 'index.scss'),
-          path.join('node_modules', normalizedDir, 'package.json'),
-          path.join('node_modules', normalizedDir, normalizedFile, 'package.json'),
+          path.join(prefix, normalizedDir, `${normalizedFile}.scss`),
+          path.join(prefix, normalizedDir, `_${normalizedFile}.scss`),
+          path.join(prefix, normalizedUrl, 'index.scss'),
+          path.join(prefix, normalizedDir, 'package.json'),
+          path.join(prefix, normalizedDir, normalizedFile, 'package.json'),
         ];
 
         const file = findup(checkFor, {
@@ -40,33 +44,41 @@ module.exports = (entry, dest, options) => {
           const pkg = require(file);
           const relativeStyle = pkg.style || pkg.main || 'index.scss';
 
-          return done({ file: path.resolve(path.dirname(file), relativeStyle) });
+          return done({
+            file: path.resolve(path.dirname(file), relativeStyle),
+          });
         }
 
         return done({ file });
-      }
-      return null;
+        // }
+        // return null;
+      },
     },
-  }, (sassErr, sassResult) => {
-    if (!sassErr) {
-      postcss(params.plugins)
-        .process(sassResult.css, { map: options.sourceMap, from: entry, to: dest })
-        .then((postcssResult) => {
-          mkdirp(path.dirname(dest), (mkdirpErr) => {
-            if (mkdirpErr) {
-              console.error(mkdirpErr);
-              process.exit(1);
-            } else {
-              fs.writeFile(dest, postcssResult.css);
-              if (postcssResult.map) {
-                fs.writeFile(`${dest}.map`, postcssResult.map);
+    (sassErr, sassResult) => {
+      if (!sassErr) {
+        postcss(params.plugins)
+          .process(sassResult.css, {
+            map: options.sourceMap,
+            from: entry,
+            to: dest,
+          })
+          .then(postcssResult => {
+            mkdirp(path.dirname(dest), mkdirpErr => {
+              if (mkdirpErr) {
+                console.error(mkdirpErr);
+                process.exit(1);
+              } else {
+                fs.writeFile(dest, postcssResult.css);
+                if (postcssResult.map) {
+                  fs.writeFile(`${dest}.map`, postcssResult.map);
+                }
               }
-            }
+            });
           });
-        });
-    } else {
-      console.error(sassErr);
-      process.exit(1);
+      } else {
+        console.error(sassErr);
+        process.exit(1);
+      }
     }
-  });
+  );
 };
