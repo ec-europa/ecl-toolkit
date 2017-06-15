fc7f874888ec248e591d17a8575e0b43acb9e666
Yannick Huard <mail@yannickhuard.com>
2017-06-15 10:12:29 +0200

    Add some more doc

--- examples/basic/framework/index.js
+++ examples/basic/framework/index.js
@@ -1,5 +1,10 @@
-// Import polyfills
-import 'core-js';
+// 1. Import polyfills (if needed)
 
-// Export components
+// 1.a Either import the whole polyfills library (not recommended):
+// import 'core-js';
+
+// 1.b Or import  only the polyfills you need (recommended), like:
+// import 'core-js/fn/array/from';
+
+// 2. Export components
 export * from './components/expandables/expandables';
