0c0720688503a36bb41bba36113cfa3a01795187
Yannick Huard <mail@yannickhuard.com>
2017-07-21 15:16:36 +0200

    chore(deps): update dependencies

--- packages/ecl-components-preset-base/index.js
+++ packages/ecl-components-preset-base/index.js
@@ -2,8 +2,11 @@
 
 export * from '@ec-europa/ecl-accordions/accordions';
 export * from '@ec-europa/ecl-dropdowns/ecl-dropdowns';
+export * from '@ec-europa//ecl-dialogs/dialogs';
 export * from '@ec-europa/ecl-expandables/expandables';
+export * from '@ec-europa//ecl-forms-file-uploads/ecl-forms-file-uploads';
 export * from '@ec-europa/ecl-messages/messages';
+export * from '@ec-europa//ecl-navigation-inpages/ecl-navigation-inpages';
 export * from '@ec-europa/ecl-navigation-menus/megamenu';
 export * from '@ec-europa/ecl-tables/ecl-tables';
 export * from '@ec-europa/ecl-tabs/tabs';
