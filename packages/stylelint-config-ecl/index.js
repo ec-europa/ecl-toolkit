e1bc42f0b6d138dba92a7ab4b5b35e9bc6813c2b
Yannick Huard <mail@yannickhuard.com>
2017-07-21 15:17:26 +0200

    chore(deps): update dependencies (#41)

--- packages/stylelint-config-ecl/index.js
+++ packages/stylelint-config-ecl/index.js
@@ -2,17 +2,14 @@
 function bemSelector(block) {
   const ns = 'ecl-';
   const WORD = '[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*';
-  const element = '(?:__' + WORD + ')?';
-  const modifier = '(?:--' + WORD + '){0,2}';
+  const element = `(?:__${WORD})?`;
+  const modifier = `(?:--${WORD}){0,2}`;
   const attribute = '(?:\\[.+\\])?';
-  return new RegExp('^\\.' + ns + block + element + modifier + attribute + '$');
+  return new RegExp(`^\\.${ns}${block}${element}${modifier}${attribute}$`);
 }
 
 module.exports = {
-  extends: [
-    'stylelint-config-sass-guidelines',
-    'stylelint-config-standard',
-  ],
+  extends: ['stylelint-config-sass-guidelines', 'stylelint-config-standard'],
   plugins: ['stylelint-selector-bem-pattern'],
   rules: {
     'plugin/selector-bem-pattern': {
@@ -20,5 +17,19 @@ module.exports = {
       componentSelectors: bemSelector,
     },
     'selector-class-pattern': null,
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'at-root',
+          'debug',
+          'error',
+          'extend',
+          'import-normalize',
+          'include',
+          'warn',
+        ],
+      },
+    ],
   },
 };
