8b1efd892106f44a066ffbbeaa9c4c7fb030fb85
Yannick Huard <mail@yannickhuard.com>
2017-08-01 14:12:24 +0200

    chore(package): update dependencies

--- packages/ecl-components-preset-base/index.js
+++ packages/ecl-components-preset-base/index.js
@@ -1,12 +1,15 @@
-// 2. Export components
+// Export components
 
-export * from '@ec-europa/ecl-accordions/accordions';
-export * from '@ec-europa/ecl-dropdowns/ecl-dropdowns';
-export * from '@ec-europa//ecl-dialogs/dialogs';
-export * from '@ec-europa/ecl-expandables/expandables';
-export * from '@ec-europa//ecl-forms-file-uploads/ecl-forms-file-uploads';
-export * from '@ec-europa/ecl-messages/messages';
-export * from '@ec-europa//ecl-navigation-inpages/ecl-navigation-inpages';
-export * from '@ec-europa/ecl-navigation-menus/megamenu';
-export * from '@ec-europa/ecl-tables/ecl-tables';
-export * from '@ec-europa/ecl-tabs/tabs';
+export * from '@ec-europa/ecl-accordions';
+export * from '@ec-europa/ecl-context-navs';
+export * from '@ec-europa/ecl-dropdowns';
+export * from '@ec-europa/ecl-dialogs';
+export * from '@ec-europa/ecl-expandables';
+export * from '@ec-europa/ecl-forms-file-uploads';
+export * from '@ec-europa/ecl-lang-select-pages';
+export * from '@ec-europa/ecl-messages';
+export * from '@ec-europa/ecl-navigation-inpages';
+export * from '@ec-europa/ecl-navigation-menus';
+export * from '@ec-europa/ecl-tables';
+export * from '@ec-europa/ecl-tabs';
+export * from '@ec-europa/ecl-timelines';
