818f80aa62d207c302cc86511bbc80f2e94501cd
Yannick Huard <mail@yannickhuard.com>
2017-12-20 16:08:49 +0100

    feat(theme): improve responsive behaviour - INNO-867

--- packages/ecl-fractal-theme/assets/js/ecl-fractal-theme.js
+++ packages/ecl-fractal-theme/assets/js/ecl-fractal-theme.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies, import/first */
 import $ from 'jquery';
-
+import * as ECL from '@ec-europa/ecl-preset-full';
 // Local imports
 import framer from './components/frame';
 import Tree from './components/tree';
@@ -13,6 +13,8 @@ document.addEventListener('DOMContentLoaded', () => {
   $.map($('[data-behaviour="tree"]'), t => new Tree(t));
   $.map($('[data-behaviour="pen"]'), p => new Pen(p));
   search();
+
+  ECL.megamenu();
 });
 
 export default events;
