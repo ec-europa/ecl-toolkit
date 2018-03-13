f18fb9f23c70a021a2858672834d8436c5560d9a
Yannick Huard <mail@yannickhuard.com>
2018-03-13 08:51:03 +0100

    feat(sass): remove custom importer - INNO-973

--- packages/ecl-builder/scripts/styles.js
+++ packages/ecl-builder/scripts/styles.js
@@ -6,22 +6,9 @@ const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer');
 const postcssNormalize = require('postcss-normalize');
 const mkdirp = require('mkdirp');
-const findup = require('findup-sync');
 
 const handleError = err => {
   if (err) throw err;
-};
-
-const loadFile = (file, done) => {
-  // Special behavior: embed CSS files
-  if (path.extname(file) === '.css') {
-    return fs.readFile(file, 'utf8', (err, data) => {
-      if (err) throw err;
-      return done({ contents: data });
-    });
-  }
-
-  return done({ file });
 };
 
 module.exports = (entry, dest, options) => {
@@ -45,41 +32,10 @@ module.exports = (entry, dest, options) => {
       sourceMap: options.sourceMap === true,
       sourceMapContents: options.sourceMap === true,
       sourceMapEmbed: options.sourceMap === true,
-      importer: (url, prev, done) => {
-        try {
-          const base = path.dirname(prev);
-          const normalizedUrl = url.startsWith('~') ? url.substr(1) : url;
-          const normalizedDir = path.dirname(normalizedUrl);
-          const normalizedFile = path.basename(normalizedUrl, '.scss');
-          const prefix = url.startsWith('~') ? 'node_modules' : '';
-
-          const checkFor = [
-            path.join(prefix, normalizedDir, `${normalizedFile}.scss`),
-            path.join(prefix, normalizedDir, `_${normalizedFile}.scss`),
-            path.join(prefix, normalizedDir, `${normalizedFile}.css`),
-            path.join(prefix, normalizedUrl, 'index.scss'),
-            path.join(prefix, normalizedDir, 'package.json'),
-            path.join(prefix, normalizedDir, normalizedFile, 'package.json'),
-          ];
-
-          const file = findup(checkFor, { cwd: base });
-
-          if (path.basename(file) === 'package.json') {
-            // eslint-disable-next-line
-            const pkg = require(file);
-            const relativeStyle = pkg.style || pkg.main || 'index.scss';
-            return loadFile(
-              path.resolve(path.dirname(file), relativeStyle),
-              done
-            );
-          }
-
-          return loadFile(file, done);
-        } catch (e) {
-          // Return a better error message?
-          return done();
-        }
-      },
+      includePaths: [
+        path.resolve(process.cwd(), 'node_modules'),
+        ...(options.includePaths || []),
+      ],
     },
     (sassErr, sassResult) => {
       if (sassErr) throw sassErr;
