91a1feaaacfd48aa335363bd650ffa17ae46a7eb
Stavros Kounis <skounis@gmail.com>
2019-05-10 12:09:24 +0200

    feat(build/style)
    resolve path/files and build

--- packages/builder/bin/build.js
+++ packages/builder/bin/build.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const glob = require('glob');
+const globBase = require('glob-base');
 const path = require('path');
 const program = require('commander');
 const buildScript = require('../scripts/scripts');
@@ -11,6 +13,32 @@ const loadConfig = configFile => {
   const conf = configFile || 'ecl-builder.config.js';
   return require(path.resolve(process.cwd(), conf)); // eslint-disable-line
 };
+
+const target = function(source, globBase, dest) {
+  const SOURCE_EXT = '.scss';
+  const TARGET_EXT = '.css';
+
+  // If there is no glob path detected return the destination.
+  if (!globBase.isGlob) { return dest; }
+  
+  source = source.replace(globBase.base, dest);
+  source = source.replace(SOURCE_EXT, TARGET_EXT);
+  return source;
+}
+
+const resolveAndBuildStyles = function(conf) {
+  const gb = globBase(conf.entry);
+
+  glob(conf.entry, {}, function (er, files) {
+    files = files.filter(file => {
+      const basename = path.basename(file);
+      return !basename.startsWith('_');
+    }).forEach(file => {
+      const dest = target(file, gb, conf.dest);
+      buildStyles(file, dest, conf.options)
+    })
+  });
+}
 
 program
   .version(pkg.version)
@@ -36,7 +64,7 @@ program
   .action(() => {
     const config = loadConfig(program.config);
     config.styles.forEach(conf =>
-      buildStyles(conf.entry, conf.dest, conf.options)
+      resolveAndBuildStyles(conf);
     );
   });
 
